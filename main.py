a82bd17a212aea05e2a3328138e385d6aacd47c6
Alex <alexmal2804@gmail.com>
2020-12-03 15:33:48 +0300

    2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas as pd
 import Phystech
 import string
 
-
+users: Phystech = []
 def generate_df() -> object:
     # Use a breakpoint in the code line below to debug your script.
     """Задание с двумя звёздочками -- сгенерировать 20 случайных пользователей, случайным образом
@@ -15,15 +15,16 @@ def generate_df() -> object:
     сколько у разных пользователей общих друзей, а потом визуализировать эти данные в seaborn с помощью line plots и heatmap
     соответственно."""
     # Создаем пользователей
-    users: Phystech = []
-    for i in range(rnd.randint(1000, 10000)):
+    for i in range(rnd.randint(10, 100)):
         first_name: str = "{0}{1}".format(str(string.ascii_uppercase[rnd.randint(0, 22)]), "".join(
             [string.ascii_lowercase[rnd.randint(0, 22)] for i in range(rnd.randint(3, 12))])) + " "
         second_name: str = "{0}{1}".format(str(string.ascii_uppercase[rnd.randint(0, 22)]), "".join(
             [string.ascii_lowercase[rnd.randint(0, 22)] for i in range(rnd.randint(3, 12))]))
         full_name = first_name + second_name
-        login: str =  "{0}{1}".format(str(string.ascii_uppercase[rnd.randint(0, 22)]), "".join(
-            [string.ascii_lowercase[rnd.randint(0, 22)] for i in range(rnd.randint(3, 12))]))
-        users.append(Phystech(full_name, ))
+        login: str = "".join([string.ascii_lowercase[rnd.randint(0, 22)] for i in range(rnd.randint(3, 12))])
+        password: str = "".join([string.ascii_letters[rnd.randint(0, 22)] for i in range(rnd.randint(3, 12))])
+
+        users.append(Phystech.Phystech(full_name, login, password))
+    print(users)
 if __name__ == '__main__':
     generate_df()
