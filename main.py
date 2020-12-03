68d3b4271e15295bd492ef7b88f14b341f14da63
Alex <alexmal2804@gmail.com>
2020-12-03 15:24:45 +0300

    2

--- main.py
+++ main.py
@@ -15,12 +15,15 @@ def generate_df() -> object:
     сколько у разных пользователей общих друзей, а потом визуализировать эти данные в seaborn с помощью line plots и heatmap
     соответственно."""
     # Создаем пользователей
+    users: Phystech = []
     for i in range(rnd.randint(1000, 10000)):
         first_name: str = "{0}{1}".format(str(string.ascii_uppercase[rnd.randint(0, 22)]), "".join(
             [string.ascii_lowercase[rnd.randint(0, 22)] for i in range(rnd.randint(3, 12))])) + " "
         second_name: str = "{0}{1}".format(str(string.ascii_uppercase[rnd.randint(0, 22)]), "".join(
             [string.ascii_lowercase[rnd.randint(0, 22)] for i in range(rnd.randint(3, 12))]))
         full_name = first_name + second_name
-
+        login: str =  "{0}{1}".format(str(string.ascii_uppercase[rnd.randint(0, 22)]), "".join(
+            [string.ascii_lowercase[rnd.randint(0, 22)] for i in range(rnd.randint(3, 12))]))
+        users.append(Phystech(full_name, ))
 if __name__ == '__main__':
     generate_df()
